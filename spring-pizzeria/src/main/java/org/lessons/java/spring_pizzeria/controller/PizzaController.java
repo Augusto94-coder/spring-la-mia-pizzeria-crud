f404c02cabd362fc18b9f7be896a04cbecdbe714
Augusto94-coder <augustodileo94@gmail.com>
2026-02-17 17:04:44 +0100

    Step 1: Layout, pizza index e home fatte

---
+++ spring-pizzeria/src/main/java/org/lessons/java/spring_pizzeria/controller/PizzaController.java
@@ -0,0 +1,30 @@
+package org.lessons.java.spring_pizzeria.controller;
+
+import java.util.List;
+
+import org.lessons.java.spring_pizzeria.model.Pizza;
+import org.lessons.java.spring_pizzeria.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+
+        List<Pizza> pizzas = repository.findAll();
+
+        model.addAttribute("pizzas", pizzas);
+
+        return "pizzas/index";
+    }
+
+}
---
+++ spring-pizzeria/src/main/java/org/lessons/java/spring_pizzeria/model/Pizza.java
@@ -0,0 +1,82 @@
+package org.lessons.java.spring_pizzeria.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+    @NotBlank(message = "Il nome di una pizza non può essere mancante/vuoto")
+    @Size(max = 70, message = "il nome della pizza non può superare i 70 caratteri")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Lob
+    private String description;
+
+    @Lob
+    private String photo;
+
+    @NotNull(message ="Il prezzo non può essere vuoto")
+    @Positive(message ="Il prezzo deve essere un numero positivo")
+    @Column(nullable = false)
+    private Double price;
+    
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return this.photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+}
+
+    
