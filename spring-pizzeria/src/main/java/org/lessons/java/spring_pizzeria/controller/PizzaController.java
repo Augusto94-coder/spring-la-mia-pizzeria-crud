690e49f742a3942f55fc92540ccea2c1881056fd
Augusto94-coder <augustodileo94@gmail.com>
2026-02-17 17:41:13 +0100

    Step 2: implementata show e pagina relativa

--- spring-pizzeria/src/main/java/org/lessons/java/spring_pizzeria/controller/PizzaController.java
+++ spring-pizzeria/src/main/java/org/lessons/java/spring_pizzeria/controller/PizzaController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @Controller
 @RequestMapping("/pizzas")
@@ -27,4 +30,11 @@ public class PizzaController {
         return "pizzas/index";
     }
 
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/show";
+    }
+    
 }
